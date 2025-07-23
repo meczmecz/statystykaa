34e212a77f4a0e0c92fb2ee0f3988a4ccfffd585
jaguar <natalkat09@gmail.com>
2025-07-23 14:01:23 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,7 @@
-// Import Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
-// Konfiguracja Twojej aplikacji
+// Konfiguracja aplikacji
 const firebaseConfig = {
   apiKey: "AIzaSyAWXiBO5-woCCEqCBc2rfOIo1RhUUtevzU",
   authDomain: "jaguar-analityka.firebaseapp.com",
@@ -14,11 +12,9 @@ const firebaseConfig = {
   measurementId: "G-T3C1M10SM6",
   databaseURL: "https://jaguar-analityka-default-rtdb.europe-west1.firebasedatabase.app" // ← dodaj to ręcznie!
 };
-
 // Inicjalizacja Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 const auth = getAuth(app);
-
 // Eksportuj, aby app.js mógł korzystać
 export { db, auth };
